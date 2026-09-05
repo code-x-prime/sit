cc064acb1a3ba3af04a1b812707f0f7fd7870544
riteshk-007 <codeshorts007@gmail.com>
2026-09-05 20:18:56 +0530

    feat: initialize root layout with global components, fonts, and tracking scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,27 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${oswald.variable} ${poppins.variable}`} suppressHydrationWarning>
       <body className="relative min-h-screen bg-background font-sans text-foreground antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T4HMKVZ6"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T4HMKVZ6');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
         {/* Google tag (gtag.js) */}
         <Script src="https://www.googletagmanager.com/gtag/js?id=AW-18326954298" strategy="afterInteractive" />
         <Script id="google-tag-init" strategy="afterInteractive">
